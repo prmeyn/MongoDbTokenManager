e2b6ffe8b290624004179582441a3aea85899519
Prashansa Meyn <pm@globalpay.dk>
2024-06-23 22:42:31 +0200

    abstract the services, ensure its always a singleton

---
+++ MongoDbTokenManager/SeviceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDbTokenManager.Database;
+
+namespace MongoDbTokenManager
+{
+	public static class SeviceCollectionExtensions
+	{
+		public static void AddMongoDbTokenServices(this IServiceCollection services)
+		{
+			services.AddSingleton<MongoDbTokenService>();
+		}
+	}
+}

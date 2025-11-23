e7a2142c30b6f070063a42882f4592a0aa196134
tony <ajrmeyn@gmail.com>
2025-11-23 14:01:01 +0100

    test: add token expiration test case

--- MongoDbTokenManager.Tests/TokenServiceTests.cs
+++ MongoDbTokenManager.Tests/TokenServiceTests.cs
@@ -56,4 +56,47 @@ public class TokenServiceTests
             await mongoService.Database.Client.DropDatabaseAsync(databaseName);
         }
     }
+
+    [Fact]
+    public async Task GenerateToken_ExpiresAfterValidityPeriod()
+    {
+        // Arrange
+        var connectionString = Environment.GetEnvironmentVariable("MONGODB_CONNECTION_STRING") ?? "mongodb://localhost:27017";
+        var databaseName = "TokenManagerTestDb_" + Guid.NewGuid();
+        
+        var myConfiguration = new Dictionary<string, string>
+        {
+            {"MongoDbSettings:ConnectionString", connectionString},
+            {"MongoDbSettings:DatabaseName", databaseName}
+        };
+
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(myConfiguration)
+            .Build();
+
+        var mongoService = new MongoService(configuration, NullLogger<MongoService>.Instance);
+        var tokenService = new MongoDbTokenService(mongoService);
+        var tokenId = new TokenIdentifier("test-user-expiration");
+        var logId = "test-log-id-expiration";
+
+        try
+        {
+            // Act
+            var token = await tokenService.Generate(logId, tokenId, 1, 6); // 1 second validity
+
+            // Assert
+            Assert.NotNull(token);
+            
+            // Wait for expiration
+            await Task.Delay(2000);
+
+            var isValid = await tokenService.Validate(tokenId, token);
+            Assert.False(isValid, "Token should be invalid after expiration period");
+        }
+        finally
+        {
+            // Cleanup
+            await mongoService.Database.Client.DropDatabaseAsync(databaseName);
+        }
+    }
 }
